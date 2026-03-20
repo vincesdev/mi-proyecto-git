3f221795525beb9fc9f4d11cbff3a68c95e6f617
Brivinces <josselyn.gv27@gmail.com>
2026-03-20 12:32:11 -0500

    Probando el comando git diff

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 print("Hello, Git!")
 print("This is a simple Python script to demonstrate Git version control.")
 print("Ahora estoy aprendiendo a usar las versiones en Git.")
+print("Aprendiendo git diff")
